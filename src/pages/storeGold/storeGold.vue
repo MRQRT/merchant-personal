2afe59eec82015346865dc85db78853e3cb29344
wangyanxia <1069463631@qq.com>
2018-11-01 11:36:56 +0800

    清除渠道标示缓存

--- src/pages/storeGold/storeGold.vue
+++ src/pages/storeGold/storeGold.vue
@@ -408,8 +408,8 @@ export default {
 		(invitedBy)?sessionStorage.setItem('invitedBy',invitedBy):'';
 		var tg = this.$route.query.source;
 		var yw = this.$route.query.channel;
-		(tg)?setStore('tg',tg,'local'):'';
-		(yw)?setStore('yw',yw,'local'):'';
+		(tg)?setStore('tg',tg,'local'):setStore('tg','','local');
+		(yw)?setStore('yw',yw,'local'):setStore('yw','','local');
 		const url = window.location.href;
 		const ag = /hjgjdd/.test(url);
 		if(ag){
